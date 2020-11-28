d62d162e4772dba44fb3d0597cdcdf9770a73cfc
Josh Kinzel <kinzelj008@gmail.com>
2020-11-28 15:15:47 -0600

    add h5 export feature to cnn model class

--- fma_model_builder/build_model.py
+++ fma_model_builder/build_model.py
@@ -1,12 +1,13 @@
 from genreml.model.cnn import cnn  
-from genreml.model.cnn import dataset as ds  
+from genreml.model.processing import dataset as ds  
 from fma_model_builder import custom_model as cm
 from fma_model_builder import dataset_config as config
 
 def build_model():
     dataset = ds.Dataset(config.DatasetConfig)
     keras_model = cm.CustomModel()
-    model = cnn.CnnModel().train_new_model(keras_model.model, dataset, batch_size=8, epochs=5)
+    model = cnn.CnnModel().train_new_model(name='test_model', model=keras_model.model, dataset=dataset, batch_size=8, epochs=1)
+    model.export_h5()
 
 if __name__ == '__main__':
     build_model()
